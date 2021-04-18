79ad36b922915f87d541f2ec6db93c15c78c8eca
jessicap5159 <jessicaj5159@gmail.com>
2021-04-18 16:03:55 -0700

    changed function in index.js to send data to sidebar

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -170,7 +170,7 @@ const renderNoteList = async (notes) => {
 };
 
 // Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
+const getAndRenderNotes = () => getNotes().then(data => renderNoteList(data));
 
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
